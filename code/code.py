93bea55652940c47c318471ad47b35f5b1fe84c9
zhenglian99 <44041620+zhenglian99@users.noreply.github.com>
2018-12-02 21:18:15 -0800

    Update code.py

--- code/code.py
+++ code/code.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from gensim.models import Word2Vec
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPRegressor
@@ -11,22 +12,20 @@ from datetime import date, timedelta
 
 dataset_file_name1 = '../datasets/tesla-tweets-18-1-1to18-11-27.csv'
 dataset_file_name2 = '../datasets/tesla-stocks-18-1-1to18-11-27.csv'
-dataset_file_name3 = '../datasets/tesla-average-tweets-sentiment-18-1-1to18-11-27.csv'
 
-print 'Loading Tesla tweets and sentiments dataset and Tesla stocks dataset...'
+print('Loading Tesla tweets and Tesla stocks dataset...')
 tweet_dataset = pd.read_csv(dataset_file_name1)
 stock_price_dataset = pd.read_csv(dataset_file_name2)
-tweet_sentiment_dataset = pd.read_csv(dataset_file_name3)
 
 #################################################################################################################
 '''
 In this section, we will process |tweet_dataset| to obtain a list of lists of tuples, |tweet_data|. Each list within |tweet_data|
 corresponds to a particular date, with the first element of |tweet_data| corresponding to 1/1/18 and the last element
-corresponding to the 11/27/18. Each tuple within the list contains a tweet and that tweet's number of replies, 
+corresponding to the 11/27/18. Each tuple within the list contains a tweet and that tweet's number of replies,
 number of retweets, and number of favorites, i.e. (tweet, #replies, #retweets, #favorites).
 '''
 
-print 'Processing tweets dataset...'
+print ('Processing tweets dataset...')
 def convert_to_int(input):
     '''
     A custom method to convert #replies, #retweets, #favorites to floats from |tweet_dataset|, since they come in the form
@@ -45,17 +44,12 @@ replies = tweet_dataset['replies']
 retweets = tweet_dataset['retweets']
 favorites = tweet_dataset['favorites']
 
-sentiment = tweet_sentiment_dataset['Score']
-
 prev_date = dates[0].split()[0]
-prev_i = 0
 
 for i in range(len(dates)):
     curr_date = dates[i].split()[0]
 
     if curr_date != prev_date:
-        tweet_data[len(tweet_data) - 1].append(sentiment[prev_i])
-        prev_i += 1
         tweet_data.append([])
         prev_date = curr_date
 
@@ -65,18 +59,18 @@ for i in range(len(dates)):
     num_favorites = convert_to_int(favorites[i])
     tweet_data[len(tweet_data) - 1].append((tweet, num_replies, num_retweets, num_favorites))
 
-print '{} days worth of tweet data read...'.format(len(tweet_data))
+print ('{} days worth of tweet data read...'.format(len(tweet_data)))
 #################################################################################################################
 '''
-In this section we will process |stock_price_dataset| to obtain a list of tuples, |stock_price_data|. Each tuple contains a closing stock 
-price and volume for a particular date. Each tuple within |stock_price_data| also corresponds to a particular date, with the 
-first element of |stock_price_data| corresponding to 1/1/18 and the last element corresponding to the 11/27/18. Note that the 
+In this section we will process |stock_price_dataset| to obtain a list of tuples, |stock_price_data|. Each tuple contains a closing stock
+price and volume for a particular date. Each tuple within |stock_price_data| also corresponds to a particular date, with the
+first element of |stock_price_data| corresponding to 1/1/18 and the last element corresponding to the 11/27/18. Note that the
 stock market is closed on holidays and weekends, therefore we will estimate missing stock prices and volumes
-via (x + y)/2, where x is the last known stock price, and y is the next available stock price. 
+via (x + y)/2, where x is the last known stock price, and y is the next available stock price.
 Form: (stock_price, stock_volume)
 '''
 
-print 'Processing stocks dataset...'
+print ('Processing stocks dataset...')
 stock_price_data = []
 
 dates = stock_price_dataset['Date']
@@ -118,40 +112,39 @@ for i in range(1, len(dates)):
 stock_price_data.pop(0)
 stock_price_data.pop(0)
 
-print "{} days worth of stock data read...".format(len(stock_price_data))
+print ("{} days worth of stock data read...".format(len(stock_price_data)))
 #################################################################################################################
 '''
-Here we will construct a consolidated dataset, |future_X_data| and |future_Y_data|. |future_X_data| will be a list of tuples. Each tuple 
-will be of the form tuple(|tweet_data[i:i+M]| + |stock_price_data[i:i+M]|). |future_Y_data| will be a list of our stock prices, starting 
+Here we will construct a consolidated dataset, |pastM_dataX| and |next_price_dataY|. |pastM_dataX| will be a list of tuples. Each tuple
+will be of the form tuple(|tweet_data[i:i+M]| + |stock_price_data[i:i+M]|). |next_price_dataY| will be a list of our stock prices, starting
 from |stock_price_data[i+M][0]|. i.e., we are developing the infrastructure for an auto-regressive model of memory |M|.
 '''
 
 M = 5
 
-future_X_data = []
-future_Y_data = []
+pastM_dataX = []
+next_price_dataY = []
 
 for i in range(len(tweet_data) - M):
-    future_X_data.append(tuple(tweet_data[i:i+M] + stock_price_data[i:i+M]))
-    future_Y_data.append(stock_price_data[i+M][0])
+    pastM_dataX.append(tuple(tweet_data[i:i+M] + stock_price_data[i:i+M]))
+    next_price_dataY.append(stock_price_data[i+M][0])
 
 
 #####################################################################################################################################################################
 '''
-Here we will declare methods to extract features from each tuple in |future_X_data|. Note these features 
-are written under the assumption that each tuple is in the form (tweet_info_0, ..., tweet_info_M, (close_price_0, volume_0), 
-..., (close_price_M, volume_M)). Each tweet_info_i is a list of tuples, where each tuple is of the form 
-(tweet, #replies, #retweets, #favorites). 
-
-|features| is a list of functions. Each function takes an element of |future_X_data| in the format described above and outputs
-feature value. 
+Here we will declare methods to extract features from each tuple in |pastM_dataX|. Note these features
+are written under the assumption that each tuple is in the form (tweet_info_0, ..., tweet_info_M, (close_price_0, volume_0),
+..., (close_price_M, volume_M)). Each tweet_info_i is a list of tuples, where each tuple is of the form
+(tweet, #replies, #retweets, #favorites).
+|features| is a list of functions. Each function takes an element of |pastM_dataX| in the format described above and outputs
+feature value.
 '''
 
 features = []
 
 def compute_phi(x):
     '''
-    Computes the feature vector |phi| from the datapoint |x| and returns it. 
+    Computes the feature vector |phi| from the datapoint |x| and returns it.
     '''
     phi = []
     for phi_i in features:
@@ -160,7 +153,7 @@ def compute_phi(x):
 
 #####################################################
 '''
-Create features here and describe them. Append them to |features| in the order they were created. 
+Create features here and describe them. Append them to |features| in the order they were created.
 '''
 
 #Add a bias feature
@@ -168,53 +161,53 @@ def bias(x):
     return 1
 features.append(bias)
 
-def sentiment(x):
-    return x[M - 1][len(x[M-1]) - 1]
-features.append(sentiment)
-
-#Add values of the stocks from the previous |M| days as features.
-for i in range(M, len(future_X_data[0])):
-    def stock_val(x):
+# Add values of the stocks from the previous |M| days as features.
+for i in range(M, len(pastM_dataX[0])):
+    def stock_val(x, i=i):
         return x[i][0]
     features.append(stock_val)
 
 #Add volumes of the stocks from the previous |M| days as features.
-for i in range(M, len(future_X_data[0])):
-    def volume(x):
-        return x[i][1]
-    features.append(volume)
+# for i in range(M, len(pastM_dataX[0])):
+#     def volume(x, i=i):
+#         return x[i][1]
+#     features.append(volume)
 
 #Proposed features to add for each day and reason
-    #Proposed features for each tweet and reason
-        #Each feature will be multiplied by (#replies + #retweets + #favorites) to scale for relevance
-            #maybe quantify the tweet by repeating it (#replies + #retweets + #favorites)... might not be 
-            #the greatest idea if we have to repeat the tweet 1000 times
-        #(word count)... more words means usually means a big announcement
-        #contains word increase
-        #contains word rise
-        #contains word fraud
-        #contains word bankrupt
-        #contains word fail
-        #weighted n-gram of all tweets
-        #look up word2vec
-            #for above features 'contains word', just compute the similarity of each word
-            #in a tweet to them
-        #some multiplication of these features?
+    #Standard dev?
 
-for i in range(M, len(future_X_data[0])):
+model = Word2Vec.load('../datasets/w2v_tweets.bin')
+word_list = ["increase", "decrease", "bankrupt", "fraud", "love"]
+word_and_similars_list = []
+for i in range(len(word_list)):
+    similars = model.wv.most_similar(word_list[i], topn=5)
+    similars.append((word_list[i], 1.0))
+    word_and_similars_list.append(similars)
+
+for i in range(len(word_and_similars_list)):
+    similars = word_and_similars_list[i]
+    def totalValue(x):
+        value = 0.0
+        for word_value in similars:
+            for j in range(M):
+                for tweet in x[j]:
+                    value += tweet[0].count(word_value[0]) * sum(tweet[1:]) * word_value[1] * len(tweet[0])
+        return value
+    features.append(totalValue)
+
+for i in range(M, len(pastM_dataX[0])):
     pass
 
 ############################################################################################################################
 '''
-Using these feature extractors, we will construct feature vectors, |phi|, where |phi| is a list of feature values and 
-then construct a new dataset |dataPHI|. |dataPHI| will be directly fed into our proposed learning models. 
-
+Using these feature extractors, we will construct feature vectors, |phi|, where |phi| is a list of feature values and
+then construct a new dataset |dataPHI|. |dataPHI| will be directly fed into our proposed learning models.
 We also normalize each feature vector |phi| via standardization because the scales for each feature may be very different.
 '''
 
-print 'Computing dataset feature vectors...'
+print ('Computing dataset feature vectors...')
 dataPHI = []
-for x in future_X_data:
+for x in pastM_dataX:
     phi = compute_phi(x)
     dataPHI.append(phi)
 
@@ -233,12 +226,12 @@ we will allocate the former |TRAIN_SIZE|*100% to be the train set. From the trai
 to determine the generality of our models later on.
 '''
 
-TRAIN_SIZE = 0.9
+TRAIN_SIZE = 0.85
 TEST_SIZE = 1 - TRAIN_SIZE
 
 
 print ('Splitting data into train ({}%) and test ({}%) sets...'.format(TRAIN_SIZE*100, TEST_SIZE*100))
-dataPHI_train, dataPHI_test, dataY_train, dataY_test = train_test_split(dataPHI, future_Y_data, test_size=TEST_SIZE, random_state=0, shuffle=False)
+dataPHI_train, dataPHI_test, dataY_train, dataY_test = train_test_split(dataPHI, next_price_dataY, test_size=TEST_SIZE, random_state=0, shuffle=False)
 
 ############################################################################################################################
 '''
@@ -272,6 +265,25 @@ print('k-fold train score: {}'.format(average_kfold_train_score))
 print('k-fold dev mean squared error: {}'.format(average_kfold_dev_error))
 print('k-fold dev score: {}'.format(average_kfold_dev_score))
 
+# def percent_correct(predictions, actuals):
+#     count = 0.0
+#     for i in range(len(stock_price_data)):
+#         if stock_price_data[i][0] in actuals:
+#             previous = stock_price_data[i-1][0]
+#             index = actuals.index(stock_price_data[i][0])
+#             if (actuals[index] >= previous and predictions[index] >= previous) or (actuals[index] <= previous and predictions[index] <= previous):
+#                 count += 1
+#     return count / len(predictions)
+
+def percent_correct(predictions, actuals):
+    count = 0.0
+    previous = stock_price_data[int(len(stock_price_data) * TRAIN_SIZE)][0]
+    for i in range(len(predictions)):
+        if (actuals[i] >= previous and predictions[i] >= previous) or (actuals[i] <= previous and predictions[i] <= previous):
+            count += 1
+        previous = actuals[i]
+    return count / len(predictions)
+
 print ('Training linear regression model on entire train set...\n')
 lr = LinearRegression().fit(dataPHI_train, dataY_train)
 predY_train = lr.predict(dataPHI_train)
@@ -280,6 +292,9 @@ print ('Train mean squared error: {}'.format(mean_squared_error(predY_train, dat
 print ('Train score: {}'.format(lr.score(dataPHI_train, dataY_train)))
 print ('Test mean squared error: {}'.format(mean_squared_error(predY_test, dataY_test)))
 print ('Test score: {}'.format(lr.score(dataPHI_test, dataY_test)))
+
+print('LR train percent correct', percent_correct(predY_train, dataY_train))
+print('LR test percent correct', percent_correct(predY_test, dataY_test))
 
 ############################################################################################################################
 '''
@@ -316,22 +331,12 @@ nn = MLPRegressor(hidden_layer_sizes=(10), activation='logistic', max_iter = 100
 predY_train = nn.predict(dataPHI_train)
 predY_test = nn.predict(dataPHI_test)
 print ('Train mean squared error: {}'.format(mean_squared_error(predY_train, dataY_train)))
-print ('Train score: {}'.format(lr.score(dataPHI_train, dataY_train)))
+print ('Train score: {}'.format(nn.score(dataPHI_train, dataY_train)))
 print ('Test mean squared error: {}'.format(mean_squared_error(predY_test, dataY_test)))
-print ('Test score: {}'.format(lr.score(dataPHI_test, dataY_test)))
+print ('Test score: {}'.format(nn.score(dataPHI_test, dataY_test)))
 
-
-def percent_correct(predictions, actuals):
-    count = 0.0
-    for i in range(len(stock_price_data)):
-        if stock_price_data[i][0] in actuals:
-            previous = stock_price_data[i-1][0]
-            index = actuals.index(stock_price_data[i][0])
-            if (actuals[index] >= previous and predictions[index] >= previous) or (actuals[index] <= previous and predictions[index] <= previous):
-                count += 1
-    return count / len(predictions)
-print 'Train percent correct', percent_correct(predY_train, dataY_train)
-print 'Test percent correct', percent_correct(predY_test, dataY_test)
+print('NN train percent correct', percent_correct(predY_train, dataY_train))
+print('NN test percent correct', percent_correct(predY_test, dataY_test))
 
 ############################################################################################################################
 '''
